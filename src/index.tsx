34cfbc3074c57c0a201705c0873d45f54d5bc99f
Ильин Николай <gttravel@mail.ru>
2024-02-10 17:08:35 +0300

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
 
 const root = document.getElementById('root')
 const container = createRoot(root)
 
-container.render(<App />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
+container.render(<RouterProvider router={router} />)
